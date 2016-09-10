095641275705b583908a5e534bf1ad2445d9a174
Joshua Peek <josh@joshpeek.com>
2016-09-11 00:06:30 +0900

    Use ERB view fragments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,24 +1,4 @@
 class ApplicationController < ActionController::Base
-  # Public: GitHub's parsed GraphQL::Schema.
-  #
-  # Load this cached file from the FS. The schema should periodically be
-  # refetched via `rake schema:update`.
-  Schema = GraphQL::Schema::Loader.load(JSON.parse(File.read("#{Rails.root}/db/schema.json")))
-
-  # Public: The GraphQL::Client configured with GitHub's schema and network
-  # adapter.
-  #
-  # GraphQL::Client provides two main APIs:
-  #
-  #   # Parse GraphQL query string and return a parsed definition.
-  #   VersionQuery = Client.parse("query { version }")
-  #
-  #   # Execute query definition
-  #   data = Client.query(VersionQuery)
-  #   data.version #=> 42
-  #
-  Client = GraphQL::Client.new(schema: Schema, fetch: GitHub::HTTPAdapter)
-
   private
     # Public: Define request scoped helper method for making GraphQL queries.
     #
@@ -35,7 +15,7 @@ class ApplicationController < ActionController::Base
     # Returns a structured query result or raises if the request failed with no
     # useful data.
     def query(definition, variables = {})
-      response = Client.query(definition, variables: variables, context: client_context)
+      response = GitHub::Client.query(definition, variables: variables, context: client_context)
 
       case response
       when GraphQL::Client::SuccessfulResponse
@@ -52,7 +32,7 @@ class ApplicationController < ActionController::Base
     # Public: Useful helper method for tracking GraphQL context data to pass
     # along to the network adapter.
     def client_context
-      unless access_token = Rails.application.secrets.github_access_token
+      unless access_token = GitHub::Application.secrets.github_access_token
         # $ GITHUB_ACCESS_TOKEN=abc123 bin/rails server
         #   https://help.github.com/articles/creating-an-access-token-for-command-line-use
         fail "Missing GitHub access token"
--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,66 +1,50 @@
 class RepositoriesController < ApplicationController
-  IndexQuery = Client.parse <<-'GRAPHQL'
-    query($after: String) {
+  IndexQuery = GitHub::Client.parse <<-'GRAPHQL'
+    query {
       viewer {
-        repositories(first: 10, after: $after) {
-          totalCount
-          pageInfo {
-            hasNextPage
-          }
-          edges {
-            cursor
-            node {
-              id
-              owner {
-                login
-              }
-              name
-              isFork
-              isMirror
-              isPrivate
-              stargazers {
-                totalCount
-              }
-            }
-          }
-        }
+        ...Views::Repositories::Index::Viewer
       }
     }
   GRAPHQL
 
   def index
-    data = query(IndexQuery, after: params[:after])
+    data = query IndexQuery
 
-    if request.xhr?
-      render partial: "repositories/repositories", locals: {
-        repositories: data.viewer.repositories
-      }
-    else
-      render "repositories/index", locals: {
-        viewer: data.viewer
-      }
-    end
+    render "repositories/index", locals: {
+      viewer: data.viewer
+    }
   end
 
-  ShowQuery = Client.parse <<-'GRAPHQL'
-    query($id: ID!) {
-      node(id: $id) {
-        ... on Repository {
-          id
-          owner {
-            login
-          }
-          name
-          description
-          homepageURL
-          hasIssuesEnabled
+
+  MoreQuery = GitHub::Client.parse <<-'GRAPHQL'
+    query($after: String!) {
+      viewer {
+        repositories(first: 10, after: $after) {
+          ...Views::Repositories::Repositories::RepositoryConnection
         }
       }
     }
   GRAPHQL
 
+  def more
+    data = query MoreQuery, after: params[:after]
+
+    render partial: "repositories/repositories", locals: {
+      repositories: data.viewer.repositories
+    }
+  end
+
+
+  ShowQuery = GitHub::Client.parse <<-'GRAPHQL'
+    query($id: ID!) {
+      node(id: $id) {
+        ...Views::Repositories::Show::Repository
+      }
+    }
+  GRAPHQL
+
   def show
-    data = query(ShowQuery, id: params[:id])
+    data = query ShowQuery, id: params[:id]
 
     if repository = data.node
       render "repositories/show", locals: {
